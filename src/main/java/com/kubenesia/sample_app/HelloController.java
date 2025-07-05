d2a06f0f66b0e48232f408491f30702abd15dabf
rydrafi13 <rafiryd13@gmail.com>
2025-07-05 20:33:31 +0700

    Update code add route to hostname to get hostname running apps

--- src/main/java/com/kubenesia/sample_app/HelloController.java
+++ src/main/java/com/kubenesia/sample_app/HelloController.java
@@ -2,6 +2,8 @@ package com.kubenesia.sample_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController // This annotation marks the class as a REST controller
 public class HelloController {
@@ -14,5 +16,20 @@ public class HelloController {
     @GetMapping("/greeting") // Another endpoint for demonstration
     public String greeting() {
         return "Greetings from KubenesiApp!";
+    }
+
+    @GetMapping("/hostname") // New endpoint to display the hostname
+    public String showHostname() {
+        String hostname = "Unknown Host";
+        try {
+            // Get the local host address and then its hostname
+            InetAddress ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+        } catch (UnknownHostException e) {
+            // Log the exception if hostname cannot be determined
+            e.printStackTrace();
+            System.err.println("Could not determine hostname: " + e.getMessage());
+        }
+        return "This app is running on host: " + hostname;
     }
 }
